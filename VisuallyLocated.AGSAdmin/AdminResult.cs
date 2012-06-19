6b484b029b001d5f88cef2e2ee50934ae90ce742
Shawn <shawn.kendrot@telvent.com>
2012-06-19 14:50:34 -0600

    Add missing files

---
+++ VisuallyLocated.AGSAdmin/AdminResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VisuallyLocated.ArcGIS.Server
+{
+    public class AdminResult<T>
+    {
+        public RequestStatus Status { get; set; }
+
+        public Exception Exception { get; internal set; }
+
+        public T Result { get; set; }
+    }
+}
---
+++ VisuallyLocated.AGSAdmin/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisuallyLocated.ArcGIS.Server
+{
+    internal static class Extensions
+    {
+        internal  static void SetAdminResult<T>(this TaskCompletionSource<T> source, AdminResult<T> result)
+        {
+            if (source == null) return;
+            if (result == null) return;
+
+            if (result.Exception != null)
+            {
+                source.SetException(result.Exception);
+            }
+            else
+            {
+                source.SetResult(result.Result);
+            }
+        }
+    }
+}
